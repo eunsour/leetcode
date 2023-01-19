51336dd774e106e423dce9ddd9681675aca5b7b2
eunsour <woo569628@naver.com>
2023-01-19 10:58:56 +0900

    전체적인 구조 변경 및 csv 저장 기능 추가

--- main.py
+++ main.py
@@ -1,24 +1,69 @@
 import os
+import base64
 
-from pytz import timezone
-from datetime import datetime
+from typing import List
 
-from leetcode_ranking import LeetCodeService
-from github_utils import get_github_repo, upload_github_issue
+from utils import dateutils
+from utils.logger import info
+from service.leetcode import LeetCodeService
+from service.github import GitHubService
+
+
+GITHUB_API_TOKEN = "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"
+GITHUB_API_RAW_BASE_URL = "https://raw.githubusercontent.com"
+LEETCODE_RANKING_CSV_PATH = "outputs/leetcode_ranking.csv"
+
+
+class Ranking:
+    def __init__(self, epoch_ms: int, ranking: int) -> None:
+        self.epoch_ms = epoch_ms
+        self.ranking = ranking
+
+    def __str__(self) -> str:
+        return f"Ranking({dateutils.epoch_ms_to_date_str(self.epoch_ms)},{self.ranking})"
+
+
+class LeetCodeRankingUpload:
+    def __init__(self) -> None:
+        self.github_service = GitHubService(GITHUB_API_TOKEN)
+        self.leetcode_service = LeetCodeService()
+        self.LEETCODE_RANKING_CSV_PATH = "analysis/output/leetcode_rankings.csv"
+
+    def run(self) -> None:
+        rankings_from_github: List[Ranking] = self.get_rankings_from_github()
+        ranking_from_leetcode: Ranking = Ranking(dateutils.get_epoch_ms(), self.leetcode_service.get_ranking("eunsour"))
+        latest_rankings: List[Ranking] = rankings_from_github + [ranking_from_leetcode]
+
+        issue_title = f"LeetCode Ranking - {dateutils.get_kst_today()}"
+        upload_contents = f"LeetCode Ranking : {format(ranking_from_leetcode.ranking, ',')}등"
+
+        repo = self.github_service.get_github_repo("leetcode")
+        self.upload_rankings_csv(latest_rankings)
+        self.github_service.upload_github_issue(repo, issue_title, upload_contents)
+
+    def get_rankings_from_github(self) -> List[Ranking]:
+        content: bytes = self.github_service.get_raw_content(
+            owner="eunsour",
+            repo="leetcode",
+            file_path=LEETCODE_RANKING_CSV_PATH,
+            branch="master",
+        )
+        return [
+            Ranking(*map(int, line.split(",")))
+            for line in filter(lambda x: len(x) > 0, content.decode(encoding="ascii").split("\n"))
+        ]
+
+    def upload_rankings_csv(self, sorted_rankings: List[Ranking]) -> str:
+        return self.github_service.upload_file(
+            owner="eunsour",
+            repo="leetcode",
+            file_path=LEETCODE_RANKING_CSV_PATH,
+            base64content=base64.b64encode(
+                "\n".join(map(lambda x: f"{x.epoch_ms},{x.ranking}", sorted_rankings)).encode(encoding="ascii")
+            ).decode("ascii"),
+        )
 
 
 if __name__ == "__main__":
-    access_token = os.environ["MY_GITHUB_TOKEN"]
-    repository_name = "leetcode"
-
-    seoul_timezone = timezone("Asia/Seoul")
-    today = datetime.now(seoul_timezone)
-    today_date = today.strftime("%Y년 %m월 %d일")
-
-    ranking = LeetCodeService().get_ranking("eunsour")
-
-    issue_title = f"LeetCode Ranking - {today_date}"
-    upload_contents = f"LeetCode Ranking : {format(ranking, ',')}등"
-    repo = get_github_repo(access_token, repository_name)
-    upload_github_issue(repo, issue_title, upload_contents)
-    print("Upload Github Issue Success!")
+    LeetCodeRankingUpload().run()
+    info("Upload Github Issue Success!")
